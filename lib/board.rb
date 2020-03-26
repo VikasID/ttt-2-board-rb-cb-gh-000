0b6afc2878d97fc0ec48f42b271f4d1ca163f6ef
Vikas Tripathi <7172vt@gmail.com>
2020-03-26 08:27:32 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,13 @@
 # Define the variable board below.
-board
-board =Array.new
-board =[" "," "," "," "," "," "," "," "," "]
+board =new.Array
+
+
+board[0] =[" "]
+board[1]=[" "]
+board[2]=[" "]
+board[3]=[" "]
+board[4]=[" "]
+board[5]=[" "]
+board[6]=[" "]
+board[7]=[" "]
+board[8]=[" "]
